850e43d87e1d26cc0f19e01e58d3b6ccbfac29a5
woodliang10 <wood.liang@yijinin.com>
2023-03-08 12:31:23 +0800

    use button component

---
+++ src/components/button.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #8CA86D;
+  color: white;
+  padding: 4px 10px;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+export default Button;
