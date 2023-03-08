f0ac31a87c56080e344f58d24919c7bdf4099bf2
woodliang10 <wood.liang@yijinin.com>
2023-03-08 13:35:56 +0800

    global import button component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,9 +4,10 @@ import MDXComponents from '@theme-original/MDXComponents';
 // import more items
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import Button from "../components/button";
 
 export default {
   // list the components which should be automatically imported to all .mdx
   // files
-  ...MDXComponents, Tabs, TabItem
+  ...MDXComponents, Tabs, TabItem, Button
 };
