0bbf459c513d5df070e061ea1599ee3f4e6f4cab
dextertd <dexterdickinson@protonmail.com>
2023-04-19 19:31:10 +0100

    fix config url to allow sitemap.xml generation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Bybit API Documentation',
   tagline: '',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://bybit-exchange.github.io',
   baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
