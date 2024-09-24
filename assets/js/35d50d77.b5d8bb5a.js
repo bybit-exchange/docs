bb82121a9df9eacbfe2baae696876f63d97ded47
woodliang10 <wood.liang@yijinin.com>
2024-09-24 18:23:17 +0800

    Deploy website - based on 352881cdcaaf109f2464d63aa9c6303c933b1bb8

---
+++ assets/js/35d50d77.b5d8bb5a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_docs=self.webpackChunkdocusaurus_docs||[]).push([[2353],{99422:e=>{e.exports=JSON.parse('{"title":"V5 API","slug":"/api-explorer/v5/category/","permalink":"/docs/api-explorer/v5/category/","navigation":{"next":{"title":"Market","permalink":"/docs/api-explorer/v5/market/market"}}}')}}]);
---
+++ zh-TW/assets/js/1f391b9e.6a026f2c.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunkdocusaurus_docs=self.webpackChunkdocusaurus_docs||[]).push([[3085],{14247:(e,t,a)=>{a.r(t),a.d(t,{default:()=>m});var n=a(67294),c=a(86010),l=a(10833),r=a(35281),o=a(56866),s=a(45042),d=a(39407);const i="mdxPageWrapper_j9I6";function m(e){const{content:t}=e,{metadata:{title:a,description:m,frontMatter:u}}=t,{wrapperClassName:g,hide_table_of_contents:p}=u;return n.createElement(l.FG,{className:(0,c.Z)(g??r.k.wrapper.mdxPages,r.k.page.mdxPage)},n.createElement(l.d,{title:a,description:m}),n.createElement(o.Z,null,n.createElement("main",{className:"container container--fluid margin-vert--lg"},n.createElement("div",{className:(0,c.Z)("row",i)},n.createElement("div",{className:(0,c.Z)("col",!p&&"col--8")},n.createElement("article",null,n.createElement(s.Z,null,n.createElement(t,null)))),!p&&t.toc.length>0&&n.createElement("div",{className:"col col--2"},n.createElement(d.Z,{toc:t.toc,minHeadingLevel:u.toc_min_heading_level,maxHeadingLevel:u.toc_max_heading_level}))))))}},4631:(e,t,a)=>{a.d(t,{Z:()=>o});a(67294);var n=a(42752),c=a(65488),l=a(85162);const r=a(19521).ZP.button`
+  background-color: #8CA86D;
+  color: white;
+  padding: 4px 10px;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`,o={...n.Z,Tabs:c.Z,TabItem:l.Z,Button:r}}}]);
