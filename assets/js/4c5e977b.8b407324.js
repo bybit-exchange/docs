ce40ba4047568ad14d99faa142d40d89a4814ae1
VictorWU <dachichiwuhe@gmail.com>
2025-09-05 20:52:23 +0200

    Deploy website - based on d14e81f12b760e33bf172b445827861fce628a2c

---
+++ assets/js/4c5e977b.8b407324.js
@@ -0,0 +1,11 @@
+(self.webpackChunkdocusaurus_docs=self.webpackChunkdocusaurus_docs||[]).push([[1893],{4631:(o,r,u)=>{"use strict";u.d(r,{Z:()=>n});u(67294);var c=u(42752),s=u(65488),a=u(85162);const e=u(19521).ZP.button`
+  background-color: #8CA86D;
+  color: white;
+  padding: 4px 10px;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`,n={...c.Z,Tabs:s.Z,TabItem:a.Z,Button:e}},69604:()=>{},45603:()=>{},44616:()=>{},42480:()=>{},24654:()=>{}}]);
